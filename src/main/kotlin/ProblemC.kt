773958093ea3b87053d3bdeec1293f1c2f3101de
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-03 22:58:00 +0300

    ProblemС complete

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,12 @@
 class ProblemC {
+    fun main (){
+        val count : Int = readInt()
+        val inputList : List<Int> = readListInt().sortedDescending()
+        var out = 0
 
-    fun main() {
-
-        //ваша реализация в методе main
-
+        for (i in 0 until count step 2) {
+            out += inputList[i] - inputList[i + 1]
+        }
+        println(out)
     }
-
 }
