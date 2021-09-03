21c099ed29dfa17961a4fdf6a63d491d535a39cd
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-03 21:51:49 +0300

    ProblemA complete

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,23 @@
 class ProblemA {
+    fun main (){
+        val count : Int = readInt()
+        var inputList : List<Int>
+        var toRight: Int
+        var toLeft : Int
+        var iteration : Int
+        var x : Long
 
-    fun main() {
+        for (i in 1..count){
+            inputList = readListInt()
+            toRight = inputList[0]
+            toLeft = inputList[1]
+            iteration = inputList[2]
 
-        //ваша реализация в методе main
-
+            x = (toRight - toLeft).toLong() * (iteration / 2)
+            if (iteration % 2 == 1) {
+                x += toRight
+            }
+            println(x)
+        }
     }
-
 }
